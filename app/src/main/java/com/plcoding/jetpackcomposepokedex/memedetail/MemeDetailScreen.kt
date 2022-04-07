ad06fd037be4c91775a528bb3583dc8c0634ee66
Hasan Cihad Altay <caltayzzz@gmail.com>
2022-04-08 02:17:01 +0300

    Revert to both buttons done 2

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/memedetail/MemeDetailScreen.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/memedetail/MemeDetailScreen.kt
@@ -64,17 +64,7 @@ fun MemeDetailScreen(
     ) {
         MemeDetailTopSection(navController = navController)
         Spacer(modifier = Modifier.height(16.dp))
-        Box(
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .fillMaxWidth()
-                .fillMaxHeight(0.6f),
-            contentAlignment = Alignment.Center
-        ) {
-            MemeDetailStateWrapper(
-                memeInfo = memeInfo,
-            )
-        }
+        MemeImageScreen(memeInfo = memeInfo)
         Spacer(modifier = Modifier.height(16.dp))
         MemeTextField(boxCount = boxCount)
         Spacer(modifier = Modifier.height(8.dp))
